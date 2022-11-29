63467c0abc633d3aee61c598037f39e03d5b9b87
Eleanor Conole <48476819+EleanorSC@users.noreply.github.com>
2022-11-29 22:06:55 +0000

    lifestyle model added

--- Figures/brain_age_figure.R
+++ Figures/brain_age_figure.R
@@ -563,6 +563,240 @@ newdf <- cbind(df, newcols) %>%
 
 ###
 plot_brain_ageing_proteins <- newdf
+
+# ----------------------------#
+# LIFESTYLE
+# ----------------------------#
+
+# Making a data frame with all combinations of variables
+df <-
+  as.data.frame(expand.grid(FULL_DNAm_list, FULL_neuroimaging_list, stringsAsFactors = F)) %>%
+  dplyr::rename(DNAm = Var1,
+                metric = Var2)
+
+# Function to get summary values as tibble from a named list with the info on metric and DNAm
+model_output <- function(list) {
+  
+  metric <- list$metric
+  DNAm <- list$DNAm
+  
+  regression_summary <- summary(
+    lm(
+      scale(Neuroimaging_DNAm_lifestyle[[metric]]) ~
+        scale(st_age)
+      + sex
+      #+ site
+      #+ batch
+      #+ edited
+      #+ scale(est.icv.BAD)
+      + scale(Neuroimaging_DNAm_lifestyle[[DNAm]]),
+      data = Neuroimaging_DNAm_lifestyle
+    )
+  )
+  
+  regression_summary_tidy <- broom::tidy(regression_summary)
+  regression_summary_tidy_complete <- regression_summary_tidy %>% mutate(r2 = regression_summary$r.squared)
+  regression_summary_tidy_DNAm <- regression_summary_tidy_complete[4, c(2, 3, 5, 6)]
+  
+  return(regression_summary_tidy_DNAm)
+  
+}
+
+# Making 3 new columns to add to data frame, 1 is estimate, 1 is std. error, 1 is p.value
+newcols <- df %>%
+  split(1:nrow(.)) %>%
+  purrr::map_dfr(.f = model_output)
+
+# Now bind on the 3 cols that were created
+newdf <- cbind(df, newcols) %>%
+  
+  # Create a neurimaging modality column
+  mutate(modality = 
+           case_when(
+             metric == "Brain_age" ~ "brain ageing",
+             metric == "brain_accel" ~ "brain ageing",
+             TRUE ~ "misc")) %>%
+  
+  # Create a brain metric column
+  mutate(
+    brain_metric =
+      case_when(
+        metric == "Brain_age" ~ "brain age",
+        metric == "brain_accel" ~ "brain age acceleration",
+        TRUE ~ "misc"
+      )
+  ) %>%
+  
+  # Create pFDR column
+  group_by(brain_metric, modality) %>%
+  mutate(pFDR = p.adjust(p.value, method = "fdr")) %>%
+  # Create a column to denote where there are significant hits
+  mutate(significance =
+           case_when(p.value < 0.05 ~ "Yes",
+                     TRUE ~ "No")) %>%
+  # Create a column to denote where there are FDR significant hits
+  mutate(FDR_significance =
+           case_when(pFDR < 0.05 ~ "Yes",
+                     TRUE ~ "No")) %>%
+  # Create a column to denote whether these are DNAm or proteins
+  mutate(omic_type = "DNAm") %>%
+  mutate(model = "Model 1 (n=709)")
+
+###
+plot_brain_ageing_methylation_lifestyle <- newdf
+
+df <-
+  as.data.frame(expand.grid(FULL_proteins_list, FULL_neuroimaging_list, stringsAsFactors = F)) %>%
+  dplyr::rename(DNAm = Var1,
+                metric = Var2)
+
+# Function to get summary values as tibble from a named list with the info on metric and DNAm
+model_output <- function(list) {
+  
+  metric <- list$metric
+  DNAm <- list$DNAm
+  
+  regression_summary <- summary(
+    lm(
+      scale(Neuroimaging_DNAm_lifestyle[[metric]]) ~
+        scale(st_age)
+      + sex
+      #+ site
+      #+ batch
+      #+ edited
+      #+ scale(est.icv.BAD)
+      + scale(Neuroimaging_DNAm_lifestyle[[DNAm]]),
+      data = Neuroimaging_DNAm_lifestyle
+    )
+  )
+  
+  regression_summary_tidy <- broom::tidy(regression_summary)
+  regression_summary_tidy_complete <- regression_summary_tidy %>% mutate(r2 = regression_summary$r.squared)
+  regression_summary_tidy_DNAm <- regression_summary_tidy_complete[4, c(2, 3, 5, 6)]
+  
+  return(regression_summary_tidy_DNAm)
+  
+}
+
+# Making 3 new columns to add to data frame, 1 is estimate, 1 is std. error, 1 is p.value
+newcols <- df %>%
+  split(1:nrow(.)) %>%
+  purrr::map_dfr(.f = model_output)
+
+# Now bind on the 3 cols that were created
+newdf <- cbind(df, newcols) %>%
+  
+  # Create a neurimaging modality column
+  mutate(modality = 
+           case_when(
+             metric == "Brain_age" ~ "brain ageing",
+             metric == "brain_accel" ~ "brain ageing",
+             TRUE ~ "misc")) %>%
+  
+  # Create a brain metric column
+  mutate(
+    brain_metric =
+      case_when(
+        metric == "Brain_age" ~ "brain age",
+        metric == "brain_accel" ~ "brain age acceleration",
+        TRUE ~ "misc"
+      )
+  ) %>%
+  
+  # Create pFDR column
+  group_by(brain_metric, modality) %>%
+  mutate(pFDR = p.adjust(p.value, method = "fdr")) %>%
+  # Create a column to denote where there are significant hits
+  mutate(significance =
+           case_when(p.value < 0.05 ~ "Yes",
+                     TRUE ~ "No")) %>%
+  # Create a column to denote where there are FDR significant hits
+  mutate(FDR_significance =
+           case_when(pFDR < 0.05 ~ "Yes",
+                     TRUE ~ "No")) %>%
+  # Create a column to denote whether these are DNAm or proteins
+  mutate(omic_type = "proteomics") %>%
+  mutate(model = "Model 1 (n=709)")
+
+###
+plot_brain_ageing_proteins_lifestyle <- newdf
+
+# ----------------------------#
+# Supplementary table plot
+# ----------------------------#
+## ----------------------------#
+# Combining both sets of models to see percentage attenuation
+## ----------------------------#
+
+add_on <- plot_global_protein_lifestyle %>%
+  select(estimate, std.error, r2, p.value, pFDR) %>%
+  rename(
+    estimate_lifestyle = estimate,
+    r2_lifestyle = r2,
+    p.value_lifestyle = p.value,
+    pFDR_lifestyle = pFDR,
+    std.error_lifestyle = std.error
+  )
+
+add_on <- subset(add_on, select = -c(brain_metric))
+
+table_new <- cbind(plot_global_protein, add_on)
+
+table_new %<>% mutate(percentage_increase_decrease =
+                        100 * (estimate - estimate_lifestyle)) %>%
+  
+  # filter(FDR_significance == "Yes" & estimate < 0) %>%
+  # filter(significance == "Yes") %>%
+  
+  group_by(DNAm, brain_metric) %>%
+  
+  
+  arrange(brain_metric,
+          estimate,
+          by_group = TRUE) %>%
+  
+  mutate(
+    CI_lower =
+      estimate - (1.96 * std.error),
+    CI_upper =
+      estimate + (1.96 * std.error),
+    
+    CI_lower_lifestyle =
+      estimate_lifestyle - (1.96 * std.error_lifestyle),
+    CI_upper_lifestyle =
+      estimate_lifestyle + (1.96 * std.error_lifestyle),
+    
+    better_model =
+      case_when(r2_lifestyle > r2 ~ "Yes",
+                TRUE ~ "No")) %>%
+  select(
+    brain_metric,
+    DNAm,
+    # Model 1
+    estimate,
+    CI_lower,
+    CI_upper,
+    p.value,
+    pFDR,
+    # Model 2 
+    estimate_lifestyle,
+    CI_lower_lifestyle,
+    CI_upper_lifestyle,
+    p.value_lifestyle,
+    pFDR_lifestyle,
+    # R2
+    r2,
+    r2_lifestyle,
+    better_model,
+    percentage_increase_decrease
+  ) 
+
+
+# ----------------------------#
+# write to .csv
+# ----------------------------#
+
+
 # ----------------------------#
 # EFFECT SIZE RANGE PLOT
 # ----------------------------#
@@ -707,68 +941,104 @@ ggplot(plot4,
 # BARPLOT
 # ----------------------------#
 
+####### poor brain ageing
+
+
+plot2 <- rbind(plot_brain_ageing_methylation,
+               plot_brain_ageing_proteins) %>%
+  filter(brain_metric == "brain age" & estimate > 0 & FDR_significance == "Yes" & omic_type == "DNAm")%>%
+  arrange(estimate)
+
+sig_DNAms <- c(plot2$DNAm)
+
+
 plot4 <- rbind(plot_brain_ageing_methylation,
                plot_brain_ageing_proteins) %>% 
   
   mutate(DNAm =
            stringr::str_remove(DNAm, "_proteomics")) %>% 
   mutate(DNAm = stringr::str_replace(DNAm, "_olink", ".x")) %>% 
-  filter(brain_metric == "brain age" & estimate > 0) %>%
-    filter(
-      DNAm == "GHR"          | DNAm == "MMP.1.x" | DNAm == "CCL17" |DNAm == "MST1"    |DNAm==    "MMP1"     | DNAm ==    "MMP9"  | DNAm ==       "TPSB2"  |    
-        DNAm == "CXCL10.x" | DNAm == "SIGLEC1"     | DNAm == "ICAM5" |DNAm == "SERPIND1"|DNAm==    "LGALS3BP" | DNAm ==    "VEGFA" | DNAm ==       "CRP"    |     
-        DNAm == "SKR3"         | DNAm == "CCL18"       | DNAm == "THBS2" |DNAm == "NEP"     |DNAm==    "SELE"     | DNAm ==    "PIGR"  | DNAm ==       "MMP12"  |      
-        DNAm == "STC1"         | DNAm == "FGF.21"      | DNAm == "ACY1"  |DNAm == "PRSS2"   |DNAm==    "IGFBP4"
-    )
-
-
-##### REORDER by effect size
-
-
-
-plot4 %<>% mutate(DNAm = factor(DNAm,
-                                levels = sig_DNAms))
-
+  filter(brain_metric == "brain age" & estimate > 0) %>% 
+  filter(
+    DNAm ==       "TPSB2"  |    
+      DNAm == "CXCL10.x" | 
+      DNAm == "SIGLEC1"     | 
+      DNAm == "ICAM5" |
+      DNAm == "SERPIND1"|
+      DNAm==    "LGALS3BP" | 
+      DNAm ==    "VEGFA" | 
+      DNAm ==       "CRP"    |     
+      DNAm == "SKR3"         | 
+      DNAm == "CCL18"       | 
+      DNAm == "THBS2" |
+      DNAm == "NEP"     |
+      DNAm==    "SELE"     | 
+      DNAm ==    "PIGR"  | 
+      DNAm ==       "MMP12"  |      
+      DNAm == "STC1"         | 
+      DNAm == "FGF.21"      | 
+      DNAm == "ACY1"  |
+      DNAm == "PRSS2"   |
+      DNAm==    "IGFBP4"
+  )
 
 plot4 %<>% mutate(my_alpha = case_when(omic_type == "proteomics" ~
-                                    0.3, 
-                                    TRUE ~ 1)
-                  )
+                                         0.3, 
+                                       TRUE ~ 1))
 
-##### REMOVE NA
+
+
+order_levels <- c("TPSB2" ,      "CXCL10.x" ,
+                  "SIGLEC1"    ,  "ICAM5"    ,    
+                  "SERPIND1"   ,  "LGALS3BP"  ,   "VEGFA"      , 
+                  "CRP"        , 
+                  "SKR3"  ,   
+                  "CCL18"    ,  
+                  "THBS2"  ,    
+                  "NEP"  ,     
+                  "SELE"   ,  
+                  "PIGR"  ,  
+                  "MMP12"  ,    
+                  "STC1",
+                  "FGF.21"  ,    
+                  "ACY1" ,       "PRSS2"   ,     "IGFBP4")
+
+plot4 %<>% mutate(DNAm = factor(DNAm,
+                                levels = rev(order_levels)
+)
+)
+
+plot5 <- plot4 %>% arrange(estimate)
 
 ggplot(plot4,
        
        aes(
          x = DNAm,
          y = estimate,
-       #  fill = DNAm,
-       #  alpha = omic_type, 
-         colour = significance, 
          group = omic_type
          
        )) +
   
   geom_col(
-           aes(x = DNAm,
-               y = estimate,
-               fill = DNAm,
-               alpha = plot4$my_alpha
-               
-           ),
-           position=position_dodge2(preserve = "single")
-           ) +
-  
-  coord_flip() +
-  
-  geom_errorbar(
-    aes(
-      ymin = estimate,
-      ymax = estimate + std.error
+    aes(x = DNAm,
+        y = estimate,
+        fill = DNAm,
+        alpha = plot4$my_alpha, 
     ),
-    position = position_dodge2(.9, padding = .6, preserve = "single"),
-    #width = 0.25,
-    size = 0.6
+    colour = "black",
+    position=position_dodge2(preserve = "single")
+  ) +
+  
+  
+  geom_errorbar(data =plot4, 
+                aes(x = DNAm, 
+                    ymin = estimate + std.error*1.96,
+                    ymax = estimate
+                ),
+                position = position_dodge2(.9, 
+                                           padding = .6, 
+                                           preserve = "single"),
+                size = 0.6
   ) +
   
   theme_classic() +
@@ -782,7 +1052,7 @@ ggplot(plot4,
           colour = "black"
         ),
         axis.text.x = element_text(
-          vjust = 0.5,
+          angle = 45,
           hjust = 1,
           size = 6,
           family = "sans"
@@ -803,14 +1073,110 @@ ggplot(plot4,
     alpha = 0.5
   ) +
   
-  #  facet_wrap( ~ omic_type 
-  #             # scales = "free_y"
-  #              
-  #  ) +
+  facetSettings +
+  scale_fill_manual(values = c(
+    colorspace::sequential_hcl(26, 
+                               palette = "Purple-Blu")
+  ))
+
+####### good brain ageing
+
+
+plot2 <- rbind(plot_brain_ageing_methylation,
+               plot_brain_ageing_proteins) %>%
+  filter(brain_metric == "brain age" & estimate < 0 & significance == "Yes" & omic_type == "DNAm")%>%
+  arrange(estimate)
+
+sig_DNAms <- c(plot2$DNAm)
+
+
+plot4 <- rbind(plot_brain_ageing_methylation,
+               plot_brain_ageing_proteins) %>% 
+  
+  mutate(DNAm =
+           stringr::str_remove(DNAm, "_proteomics")) %>% 
+  mutate(DNAm = stringr::str_replace(DNAm, "_olink", ".x")) %>% 
+  filter(brain_metric == "brain age" & estimate < 0) %>% 
+filter( DNAm =="CNTN4"  | DNAm ==     "NCAM1"| DNAm == "SHBG"   | DNAm ==     "IGFBP1"| DNAm ==      "WFIKKN2"|DNAm ==     "NOTCH1"|DNAm ==      "NTRK3"       |DNAm =="OMD"    | DNAm==    "SLITRK5" |   
+        DNAm =="SEMA3E"| DNAm ==      "MIA"   | DNAm == "SELL"    | DNAm ==    "GP1BA"  | DNAm ==     "CD209"     |DNAm ==  "FCER2"     |DNAm ==  "NTRK3_olink" |DNAm =="ADAMTS13" |DNAm==   "C4A.C4B" )
+
+plot4 %<>% mutate(my_alpha = case_when(omic_type == "proteomics" ~
+                                         0.3, 
+                                       TRUE ~ 1)
+)
+plot4 %<>% mutate(DNAm = factor(DNAm,
+                                levels = sig_DNAms))
+
+plot5 <- plot4 %>% arrange(estimate)
+
+ggplot(plot4,
+       
+       aes(
+         x = DNAm,
+         y = -estimate,
+         #colour = significance, 
+         group = omic_type
+         
+       )) +
+  
+  geom_col(
+    aes(x = DNAm,
+        y = -estimate,
+        fill = DNAm,
+        alpha = plot4$my_alpha, 
+        
+        
+    ),colour = "black",
+    position=position_dodge2(preserve = "single")
+  ) +
+  
+  
+  geom_errorbar(data =plot4, 
+    aes(x = DNAm, 
+      ymax = -estimate + std.error*1.96,
+      ymin = -estimate
+    ),
+    position = position_dodge2(.9, 
+                               padding = .6, 
+                               preserve = "single"),
+    size = 0.6
+  ) +
+  
+  theme_classic() +
+  
+  theme(legend.position = "none",
+        
+        strip.text = element_text(
+          size = 6,
+          face = "bold",
+          family = "sans",
+          colour = "black"
+        ),
+        axis.text.x = element_text(
+          angle = 45,
+          hjust = 1,
+          size = 6,
+          family = "sans"
+        ),
+        axis.text.y = element_text(size = 6)
+        
+        
+  ) +
+  
+  labs(x = "",
+       y = "") +
+  
+  geom_hline(
+    yintercept = 0,
+    color = "darkgrey",
+    linetype = "dashed",
+    size = 0.2,
+    alpha = 0.5
+  ) +
   
   facetSettings +
-  
   scale_fill_manual(values = c(
-    colorspace::qualitative_hcl(26, palette = "Cold") 
+    colorspace::sequential_hcl(17, 
+                               palette = "Dark Mint") 
   )) +
   scale_colour_manual(values = c("grey", "black"))
